24d8c2fd6f8016bfb429b14c8b48a3c8958cf8f8
Pravin-Maurya <pravinmaurya0587@gmail.com>
2023-02-25 11:24:48 +0530

    filter features added

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,13 +1,19 @@
 import Table from "react-bootstrap/Table";
 import AxiosConfig from "../../axiosConfig";
 import { useEffect, useState } from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import { Button } from "react-bootstrap";
+import "./table.css";
 
 const TableData = () => {
   const [tableData, setTableData] = useState([]);
+  const [filteredData, setFilteredData] = useState(tableData);
+  const [searchInput, setSearchInput] = useState("");
+  console.log(searchInput);
   const getTableData = async () => {
     try {
       const response = await AxiosConfig.put("/api/react-test");
-      console.log(response);
+
       setTableData(response.data);
     } catch (error) {
       console.log(error);
@@ -17,28 +23,56 @@ const TableData = () => {
     getTableData();
   }, []);
 
+  useEffect(() => {
+    setFilteredData(seachName());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [searchInput]);
+
+  const seachName = () => {
+    return tableData.filter((data) =>
+      data.name.toLowerCase().includes(searchInput.toLowerCase())
+    );
+  };
+
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>S.N.</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Occupation</th>
-        </tr>
-      </thead>
-      <tbody>
-        {tableData &&
-          tableData.map((data, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{data?.name}</td>
-              <td>{data?.age}</td>
-              <td>{data?.occupation}</td>
-            </tr>
-          ))}
-      </tbody>
-    </Table>
+    <div className="tableContainer">
+      <div className="container">
+        <InputGroup className="mb-3">
+          <input
+            type="text"
+            placeholder="Search by name"
+            onChange={(event) => setSearchInput(event.target.value)}
+          />
+          <Button onClick={seachName}>Search</Button>
+        </InputGroup>
+      </div>
+      {searchInput && (
+        <div className="filterResultContainer">
+          You are viewing filtered results!
+        </div>
+      )}
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>S.N.</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Occupation</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData &&
+            filteredData.map((data, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{data?.name}</td>
+                <td>{data?.age}</td>
+                <td>{data?.occupation}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </div>
   );
 };
 export default TableData;
