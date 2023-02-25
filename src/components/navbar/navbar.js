d0a08eb9fae76b40924fdf56dd0006f99e79493a
Pravin-Maurya <pravinmaurya0587@gmail.com>
2023-02-25 14:02:19 +0530

    navbar bug fixed

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,4 +1,3 @@
-import Navbar from "react-bootstrap/Navbar";
 import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
@@ -19,53 +18,50 @@ const AppNavbar = () => {
     window.location.reload();
   };
   return (
-    <>
-      <Navbar className="navContainer">
-        <ul className="linkContainer">
+    <div className="navContainer">
+      <ul className="linkContainer">
+        <li className="linkList">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "activeClassName" : "inActiveClassName"
+            }
+          >
+            Table
+          </NavLink>
+        </li>
+        <li className="linkList">
+          <NavLink
+            to="/card-list"
+            className={({ isActive }) =>
+              isActive ? "activeClassName" : "inActiveClassName"
+            }
+          >
+            Card List
+          </NavLink>
+        </li>
+        {!userName && (
           <li className="linkList">
             <NavLink
-              to="/"
+              to="/login"
               className={({ isActive }) =>
                 isActive ? "activeClassName" : "inActiveClassName"
               }
             >
-              Table
+              Login
             </NavLink>
           </li>
-          <li className="linkList">
-            <NavLink
-              to="/card-list"
-              className={({ isActive }) =>
-                isActive ? "activeClassName" : "inActiveClassName"
-              }
-            >
-              Card List
-            </NavLink>
-          </li>
-          {!userName && (
-            <li className="linkList">
-              <NavLink
-                to="/login"
-                className={({ isActive }) =>
-                  isActive ? "activeClassName" : "inActiveClassName"
-                }
-              >
-                Login
-              </NavLink>
-            </li>
-          )}
-        </ul>
-        {userName && (
-          <div className="nameLogoutContainer">
-            <p className="userName">{userName}</p>
-            <Button onClick={logout} style={{ marginRight: "10px" }}>
-              Logout
-            </Button>
-          </div>
         )}
-      </Navbar>
-      <hr />
-    </>
+      </ul>
+      {userName && (
+        <div className="nameLogoutContainer">
+          <p className="userName">{userName}</p>
+          <Button onClick={logout} style={{ marginRight: "10px" }}>
+            Logout
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 export default AppNavbar;
